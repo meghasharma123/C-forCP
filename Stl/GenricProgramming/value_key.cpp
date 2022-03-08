b5f2d47c92390c5da98f11545de3d933fcf821f9
Megha Sharma <53375800+meghasharma123@users.noreply.github.com>
2022-03-08 11:32:15 +0530

    Program run successfully

--- Stl/GenricProgramming/value_key.cpp
+++ Stl/GenricProgramming/value_key.cpp
@@ -1,24 +1,54 @@
 /* Program to find the same shoes that is along with generic function and iterators now we want to add comperator for any data type */
 
 #include<iostream>
-class Book{
+#include<list>
+using namespace std;
+template<class FI,class T,class compare>
+FI search(FI start,FI end,T key,compare cmp)
+{
+    while(start!=end){
+      if(cmp(*start,key)){
+         return start;
+       }
+       start++;
+      }
+    return end;
+}
+           
+class Fruit{
  public:
-  String name;
+  string name;
   int price;
-  Book(){
-  }
-  Book(String name,int price){
+  Fruit(string name,int price){
     this->name = name;
     this->price = price;
   }
 };
+class FruitCmp{
+  public:
+    bool operator()(Fruit A,Fruit B){
+      if(A.name == B.name){
+         return true;
+      }
+     return false;
+    }
+};
 int main(){
-  Book b1("C++",100);
-  Book b2("Java",60);
-  Book b3("Python",90);
+  Fruit f1("Apple",100);
+  Fruit f2("Mango",60);
+  Fruit f3("Orange",90);
   
-  list<Book>l;
-  l.push_back(b1);
-  l.push_back(b2);
-  l.push_back(b3);
-  
+  list<Fruit>l;
+  l.push_back(f1);
+  l.push_back(f2);
+  l.push_back(f3);
+  Fruit fruitToFind("Apple",85);
+  FruitCmp cmp;
+  auto t = search(l.begin(),l.end(),fruitToFind,cmp);
+   if(t != l.end()){
+    cout<<"Fruit matched";
+   }
+ else{
+   cout<<"Fruit not matched";
+   }
+}
